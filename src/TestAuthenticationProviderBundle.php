12a3f8842d5fdb92b30c158e24bad2a5b340acc2
Jon Cram <webignition@gmail.com>
2023-03-28 15:10:31 +0100

    Create project skeleton

---
+++ src/TestAuthenticationProviderBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\TestAuthenticationProviderBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class TestAuthenticationProviderBundle extends AbstractBundle
+{
+}
---
+++ tests/ContainerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\TestAuthenticationProviderBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ContainerTest extends TestCase
+{
+    private ContainerInterface $container;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $kernel = new TestingKernel('test', true);
+        $kernel->boot();
+
+        $this->container = $kernel->getContainer();
+    }
+
+    public function testKernelIsBooted(): void
+    {
+        self::assertInstanceOf(ContainerInterface::class, $this->container);
+    }
+}
